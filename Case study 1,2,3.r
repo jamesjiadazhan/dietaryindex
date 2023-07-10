f4f3af258c46e15fe5ff43d364aea83d482d5373
James Jiada Zhan <jzha832@emory.edu>
2023-07-10 01:11:51 -0400

    Add files via upload

--- Case study 1,2,3.r
+++ Case study 1,2,3.r
@@ -1,12 +1,19 @@
+# remove memory
+rm(list=ls())
+
 install.packages("devtools") #If you don't have "devtools" installed already
 devtools::install_github("jamesjiadazhan/dietaryindex") # Install the package from GitHub
+
+# remove utils from library if you want to reload the package
+install.packages("/Users/james/Downloads/dietaryindex-main", repos = NULL, type = "source")
+
 # If the previous steps not working, you can try the following steps by removing the "#" marks
 # library(devtools) # Load devtools
 # install_github("jamesjiadazhan/dietaryindex")
 
 library(ggplot2)
 library(dplyr)
-library(magrittr)
+# library(magrittr)
 library(tidyr)
 
 library(dietaryindex)
@@ -26,19 +33,18 @@ data("NHANES_20052006")
 
 # DASHI from the DASH trial
 DASHI_DASH = DASHI(
-  SERV_DATA = DASH_trial, 
-  RESPONDENTID = DASH_trial$Diet_Type,
-  TOTALKCAL_DASHI = DASH_trial$Kcal,
-  VEG_SERV_DASHI = DASH_trial$Vegetables, 
-  FRT_FRTJ_SERV_DASHI = DASH_trial$Fruits_Juices, 
-  NUTSLEG_SERV_DASHI = DASH_trial$Nuts_Seeds_Legumes, 
-  LOWF_DAIRY_SERV_DASHI = DASH_trial$Lowfat_Dairy,
-  WGRAIN_SERV_DASHI = DASH_trial$Wholegrains, 
-  WHITEMEAT_SERV_DASHI = DASH_trial$Whitemeat, 
-  REDPROC_MEAT_SERV_DASHI = DASH_trial$Beef_Pork_Ham, 
-  FATOIL_SERV_DASHI = DASH_trial$Fat_Oils_salad_dressing, 
-  SNACKS_SWEETS_SERV_DASHI = DASH_trial$Snacks_Sweets,
-  SODIUM_SERV_DASHI = DASH_trial$Sodium)
+    SERV_DATA = DASH_trial, 
+    RESPONDENTID = DASH_trial$Diet_Type,
+    TOTALKCAL_DASHI = DASH_trial$Kcal, 
+    TOTAL_FAT_DASHI = DASH_trial$Totalfat_Percent, 
+    SAT_FAT_DASHI = DASH_trial$Satfat_Percent, 
+    PROTEIN_DASHI = DASH_trial$Protein_Percent, 
+    CHOLESTEROL_DASHI = DASH_trial$Cholesterol, 
+    FIBER_DASHI = DASH_trial$Fiber, 
+    POTASSIUM_DASHI = DASH_trial$Potassium, 
+    MAGNESIUM_DASHI = DASH_trial$Magnesium, 
+    CALCIUM_DASHI = DASH_trial$Calcium, 
+    SODIUM_DASHI = DASH_trial$Sodium)
 
 # MEDI for the PREDIMED trial
 MEDI_PREDIMED = MEDI(
@@ -59,7 +65,7 @@ MEDI_PREDIMED = MEDI(
 
 ## NHANES 2017-2018
 # DASHI for day 1 and day 2
-DASHI_NHANES = DASHI_NHANES_FPED(NHANES_20172018$FPED_IND, NHANES_20172018$NUTRIENT_IND, NHANES_20172018$FPED_IND2, NHANES_20172018$NUTRIENT_IND2)
+DASHI_NHANES = DASHI_NHANES_FPED(NHANES_20172018$NUTRIENT, NHANES_20172018$NUTRIENT2)
 
 # MEDI for day 1 and day 2
 MEDI_NHANES = MEDI_NHANES_FPED(FPED_IND_PATH=NHANES_20172018$FPED_IND, NUTRIENT_IND_PATH=NHANES_20172018$NUTRIENT_IND, FPED_IND_PATH2=NHANES_20172018$FPED_IND2, NUTRIENT_IND_PATH2=NHANES_20172018$NUTRIENT_IND2)
@@ -146,7 +152,7 @@ HEI2020_NHANES_0506_d1_nontoddler = HEI2020_NHANES_0506_d1 %>%
 
 # Case study 1: A comparative analysis of results derived from clinical trials (i.e., DASH and PREDIMED) juxtaposed with findings from an epidemiological study (i.e., NHANES) from 2017-2018, utilizing DASHI and MEDI dietary indexes
 x_case1 = c("DASHI_DASH_DASHlowSodium", "DASHI_DASH_DASHMedSodium", "DASHI_DASH_Control", "MEDI_PREDIMED_Med_Oliveoil", "MEDI_PREDIMED_Med_Nuts", "MEDI_PREDIMED_Control", "DASHI_NHANES", "MEDI_NHANES")
-y_case1 = c(DASHI_DASH$DASHI_ALL[2]/50, DASHI_DASH$DASHI_ALL[3]/50, DASHI_DASH$DASHI_ALL[5]/50, MEDI_PREDIMED$MEDI_ALL[1]/11,  MEDI_PREDIMED$MEDI_ALL[2]/11, MEDI_PREDIMED$MEDI_ALL[3]/11, mean(DASHI_NHANES$DASHI_ALL, na.rm=TRUE)/50, mean(MEDI_NHANES$MEDI_ALL, na.rm=TRUE)/11)
+y_case1 = c(DASHI_DASH$DASHI_ALL[2]/9, DASHI_DASH$DASHI_ALL[3]/9, DASHI_DASH$DASHI_ALL[5]/9, MEDI_PREDIMED$MEDI_ALL[1]/11,  MEDI_PREDIMED$MEDI_ALL[2]/11, MEDI_PREDIMED$MEDI_ALL[3]/11, mean(DASHI_NHANES$DASHI_ALL, na.rm=TRUE)/9, mean(MEDI_NHANES$MEDI_ALL, na.rm=TRUE)/11)
 # multiply all y values by 100 to get the percentage
 y_case1 = y_case1*100
 
@@ -166,6 +172,24 @@ ggplot(df_case1, aes(x_case1, y_case1, fill=x_case1)) +
   geom_text(aes(label = round(y_case1, 2)), vjust = -0.5, size = 3.5) +
   # rotate the x axis labels by 90 degrees
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+## plot the data with histogram with different colors for different x values
+ggplot(df_case1, aes(x_case1, y_case1, fill=x_case1)) + 
+  geom_bar(stat = "identity") + theme_bw() + 
+  labs(x = "", y = "Mean dietary index percentile", title = "DASHI and MEDI Dietary Indexes in the DASH and PREDIMED trials and NHANES 2017-2018", fill = "Dietary Index") + 
+  theme(plot.title = element_text(size=22)) +
+  theme(axis.title.x = element_blank()) +
+  theme(axis.title.y = element_text(size=18)) +
+  theme(axis.text.x = element_blank()) +
+  theme(axis.text.y = element_text(size=14)) +
+  # increase the size of the legend title
+  theme(legend.title = element_text(size=18)) +
+  # increase the size of the legend text
+  theme(legend.text = element_text(size=16)) +
+  theme(aspect.ratio = 0.5) +
+  # add numeric labels to the bars and increase their size
+  geom_text(aes(label = round(y_case1, 2)), vjust = -0.5, size = 4.5)
+
 
 # Case study 2: A time series of cross-sectional computation of the HEI2020 from 2005 to 2018
 x_case2 = c("2005-2006", "2007-2008", "2009-2010", "2011-2012", "2013-2014", "2015-2016", "2017-2018")
@@ -203,7 +227,7 @@ ggplot(df_case2_long, aes(x=x_case2, y=Mean_HEI2020, group=group, color=group)) 
 # Case study 3: Comprehensive Calculation of Multiple Dietary indexes, using the NHANES data in 2017-2018
 ## create 2 vectors for the x and y axis
 x_case3 = c("DASHI", "MEDI", "DASH", "MED", "HEI2020", "AHEI", "DII")
-y_case3 = c(mean(DASHI_NHANES$DASHI_ALL, na.rm=TRUE)/50, mean(MEDI_NHANES$MEDI_ALL, na.rm=TRUE)/11, mean(DASH_NHANES$DASH_ALL, na.rm=TRUE)/40, mean(MED_NHANES$MED_ALL, na.rm=TRUE)/9, mean(HEI2020_NHANES_1718$HEI2020_ALL, na.rm=TRUE)/100, mean(AHEI_NHANES$AHEI_ALL, na.rm=TRUE)/110, 0.75)
+y_case3 = c(mean(DASHI_NHANES$DASHI_ALL, na.rm=TRUE)/9, mean(MEDI_NHANES$MEDI_ALL, na.rm=TRUE)/11, mean(DASH_NHANES$DASH_ALL, na.rm=TRUE)/40, mean(MED_NHANES$MED_ALL, na.rm=TRUE)/9, mean(HEI2020_NHANES_1718$HEI2020_ALL, na.rm=TRUE)/100, mean(AHEI_NHANES$AHEI_ALL, na.rm=TRUE)/110, 0.75)
 # multiply all y values by 100 to get the percentage
 y_case3 = y_case3*100
 
@@ -213,13 +237,16 @@ df_case3 = data.frame(x_case3, y_case3)
 ## plot the data with histogram with different colors for different x values
 ggplot(df_case3, aes(x_case3, y_case3, fill=x_case3)) + 
   geom_bar(stat = "identity") + theme_bw() + 
-  labs(x = "Dietary Index", y = "Mean dietary index percentile", title = "Dietary Indexes in NHANES 2017-2018", fill = "Group") + 
+  labs(x = "", y = "Mean dietary index percentile", title = "Dietary Indexes in NHANES 2017-2018", fill = "Dietary Index") + 
   theme(plot.title = element_text(size=22)) +
-  theme(axis.title.x = element_text(size=18)) +
+  theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_text(size=18)) +
-  theme(axis.text.x = element_text(size=14)) +
+  theme(axis.text.x = element_blank()) +
+  theme(axis.text.y = element_text(size=14)) +
+  # increase the size of the legend title
+  theme(legend.title = element_text(size=18)) +
+  # increase the size of the legend text
+  theme(legend.text = element_text(size=16)) +
   theme(aspect.ratio = 0.5) +
   # add numeric labels to the bars
-  geom_text(aes(label = round(y_case3, 2)), vjust = -0.5, size = 3.5) +
-  # rotate the x axis labels by 90 degrees
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  geom_text(aes(label = round(y_case3, 2)), vjust = -0.5, size = 4.5)
