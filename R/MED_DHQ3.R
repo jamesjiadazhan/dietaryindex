f0a9c4a6799c8737878908c2eea1f549b3746581
James Jiada Zhan <jzha832@emory.edu>
2024-05-15 22:41:04 -0400

    Update MED_DHQ3.R

--- R/MED_DHQ3.R
+++ R/MED_DHQ3.R
@@ -43,21 +43,21 @@ MED_DHQ3 = function(DATA_PATH) {
 
     ## Create variables and functions needed for MED
     median_healthy = function(actual) {
-        median_score = median(actual)
+        median_score = median(actual, na.rm = TRUE)
         case_when(
-            actual < median_score ~ 0,
+            actual < median_score | actual == 0 ~ 0,
             actual >= median_score ~ 1
         )
     }
 
     median_unhealthy = function(actual) {
-        median_score = median(actual)
+        median_score = median(actual, na.rm = TRUE)
         case_when(
-            actual < median_score ~ 1,
+            actual < median_score | actual == 0 ~ 1,
             actual >= median_score ~ 0
         )
     }
-
+    
     COHORT %>%
         dplyr::mutate(
             MED_FRT = median_healthy(FRT_FRTJ_SERV),
