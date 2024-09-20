53fd6a4326e66c1b50560bacee3a1557d98e66d3
James Zhan <jzha832@emory.edu>
2024-09-20 14:47:27 -0400

    Citation update

--- R/zzz.r
+++ R/zzz.r
@@ -5,5 +5,5 @@
     packageStartupMessage("Tutorials: https://github.com/jamesjiadazhan/dietaryindex")
     packageStartupMessage("Dietary index calculations vary by the research question of the analysis. Currently, dietaryindex only supports simple scoring algorithm methods for all dietary indexes. Please use dietaryindex responsibly according to your research question. For more information, please refer to this resource: https://epi.grants.cancer.gov/hei/tools.html.")
     packageStartupMessage("Cite us:  citation('dietaryindex') or
-Jiada James Zhan, Rebecca A Hodge, Anne Dunlop, et al. Dietaryindex: A User-Friendly and Versatile R Package for Standardizing Dietary Pattern Analysis in Epidemiological and Clinical Studies. bioRxiv. Published online August 09, 2023:2023.08.09.548466. doi:10.1101/2023.08.07.548466")
+Zhan JJ, Hodge RA, Dunlop AL, et al. Dietaryindex: a user-friendly and versatile R package for standardizing dietary pattern analysis in epidemiological and clinical studies. Am J Clin Nutr. Published online August 23, 2024. doi:10.1016/j.ajcnut.2024.08.021")
 }
